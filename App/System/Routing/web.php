923c9c4df88859d52f4058ba4872f64e131f1dbc
IanBrison <ianbrison222@gmail.com>
2019-03-27 02:00:18 +0900

    use the new group declaration

--- App/System/Routing/web.php
+++ App/System/Routing/web.php
@@ -7,8 +7,10 @@ return [
     Router::get('/about', 'TopController', 'getAbout'),
     Router::get('/post', 'PostController', 'getIndex'),
 
-    Router::get('/admin/login', 'Admin\AuthController', 'getLogin'),
-    Router::post('/admin/login', 'Admin\AuthController', 'postLogin'),
-    Router::get('/admin/post', 'Admin\PostController', 'getNew')->withAuth('/admin/login'),
-    Router::post('/admin/post', 'Admin\PostController', 'postNew')->withAuth('/admin/login'),
+    Router::group('/admin', [
+        Router::get('/login', 'Admin\AuthController', 'getLogin'),
+        Router::post('/login', 'Admin\AuthController', 'postLogin'),
+        Router::get('/post', 'Admin\PostController', 'getNew')->withAuth('/admin/login'),
+        Router::post('/post', 'Admin\PostController', 'postNew')->withAuth('/admin/login'),
+    ]),
 ];
